1555a71d85a233f7097a509be03edae6a7595cbb
Yura Sidorov <odessa202@gmail.com>
2021-03-06 21:07:20 +0200

    Added ducks to the scene
    Added ducks to the scene

---
+++ ShootingGallery/Assets/Scripts/Target.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    Animator _animator;
+
+    void Start() => _animator = GetComponent<Animator>();
+
+    public void Hit() => _animator.SetTrigger("Hit");
+
+    public void Show() => _animator.SetTrigger("Show");
+}
---
+++ ShootingGallery/Assets/Scripts/TargetsGroup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class TargetsGroup : MonoBehaviour
+{
+    Target[] _targets;
+
+    void Start() => _targets = GetComponentsInChildren<Target>();
+
+    public void ShowRandomTarget()
+    {
+        int index = Random.Range(0, _targets.Length);
+        Target target = _targets[index];
+        target.Show();
+    }
+}
